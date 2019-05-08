f75957942d5b5f4f553cf1b0c4f909f5dbbaca57
Lene Frøkjær <lenefroekjaer@Lenes-MacBook-Pro-2.local>
2019-05-08 11:06:56 +0200

    effects when hovering

--- timeline.js
+++ timeline.js
@@ -10,6 +10,35 @@ function loadSVG() {
       document
         .querySelector("#svg_timeline")
         .insertAdjacentHTML("afterbegin", svgdata);
-      chooseEvent();
+
+      chooseEvent(); // Go to INTERACTING WITH SVG
     });
+}
+
+//// INTERACTING WITH SVG
+
+function chooseEvent() {
+  const events = document.querySelectorAll(".event");
+  events.forEach(interactivEvent);
+}
+
+function interactivEvent(event) {
+  event.addEventListener("mouseover", () => {
+    hoverEvent(event); // GO TO "HOVER OVER SVG"
+  });
+}
+
+//// HOVER OVER AN EVENT
+
+function hoverEvent(event) {
+  event.querySelector(".nail").style.fill = "#E87D4D";
+  event.querySelector("text").style.fill = "#E87D4D";
+  event.addEventListener("mouseout", () => {
+    nohoverEvent(event);
+  });
+}
+
+function nohoverEvent(event) {
+  event.querySelector(".nail").style.fill = "#494949";
+  event.querySelector("text").style.fill = "#494949";
 }
