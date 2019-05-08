8f229ccef46030b9ce99dad4e2a3830f2071ed60
Lene Frøkjær <lenefroekjaer@Lenes-MacBook-Pro-2.local>
2019-05-08 11:38:34 +0200

    clicking on an event, and some few ajustments

--- timeline.js
+++ timeline.js
@@ -1,7 +1,48 @@
 "use strict";
-window.addEventListener("DOMContentLoaded", loadSVG);
+window.addEventListener("DOMContentLoaded", loadSVG); // go to GETTING SVG
+window.addEventListener("DOMContentLoaded", loadJSON); // go to GETTING JSON
 
-//// HENTER SVG'EN
+// objects for json-data
+let Events = {
+  id: "-event-id-",
+  title: "-event-title-",
+  year: "-event-year-",
+  description: "-event-description-",
+  link1: "-event-link1-",
+  link2: "-event-link2-",
+  fromJSON(jsonObject) {
+    this.id = jsonObject.id;
+    this.title = jsonObject.title;
+    this.year = jsonObject.year;
+    this.description = jsonObject.description;
+    this.link1 = jsonObject.link1;
+    this.link2 = jsonObject.link2;
+  }
+};
+let allEvents = []; // Global: containing the json-data.
+
+//// ---- GETTING JSON
+function loadJSON() {
+  fetch("data.json").then(response =>
+    response.json().then(jsondata => {
+      prepareObjects(jsondata);
+    })
+  );
+}
+
+// preparing objects.
+function prepareObjects(jsonData) {
+  jsonData.forEach(jsonObject => {
+    //Making an new object.
+    let hcaevent = Object.create(Events);
+    hcaevent.fromJSON(jsonObject);
+
+    // saving the data in a global array.
+    allEvents.push(hcaevent);
+  });
+}
+
+//// ---- GETTING SVG
 
 function loadSVG() {
   fetch("timeline.svg")
@@ -15,7 +56,7 @@ function loadSVG() {
     });
 }
 
-//// INTERACTING WITH SVG
+//// ---- INTERACTING WITH SVG
 
 function chooseEvent() {
   const events = document.querySelectorAll(".event");
@@ -26,9 +67,14 @@ function interactivEvent(event) {
   event.addEventListener("mouseover", () => {
     hoverEvent(event); // GO TO "HOVER OVER SVG"
   });
+  event.addEventListener("click", () => {
+    // making sure that there is only one data-object displaying.
+    document.querySelector("article").innerHTML = "";
+    clickEvent(event); // go to CLICK AN EVENT
+  });
 }
 
-//// HOVER OVER AN EVENT
+//// ---- HOVER OVER AN EVENT
 
 function hoverEvent(event) {
   event.querySelector(".nail").style.fill = "#E87D4D";
@@ -41,4 +87,52 @@ function hoverEvent(event) {
 function nohoverEvent(event) {
   event.querySelector(".nail").style.fill = "#494949";
   event.querySelector("text").style.fill = "#494949";
+}
+
+//// ---- CLICK AN EVENT
+
+function clickEvent(event) {
+  allEvents.forEach(object => {
+    //only the object with the same event.id (the event that has been clicked).
+    if (object.id === event.id) {
+      placeInfo(object);
+    }
+  });
+}
+
+function placeInfo(object) {
+  // removes footer and title.
+  document.querySelector("footer").style.display = "none";
+  document.querySelector("h1").style.display = "none";
+  // showing the infoboks.
+  document.querySelector("article").style.display = "block";
+  // adjust the svg.
+  document.querySelector("svg").style.height = "60vh";
+
+  const clone = document
+    .querySelector("#event_template")
+    .content.cloneNode(true);
+
+  clone.querySelector("[data-field='title']").textContent = object.title;
+  clone.querySelector("[data-field='year']").textContent = object.year;
+  clone.querySelector("[data-field='description']").textContent =
+    object.description;
+  // preparing the links in infoboks
+  clone.querySelector("[data-field='link1']").textContent = object.link1;
+  clone.querySelector("[data-field='link1']").href = object.link1;
+  clone.querySelector("[data-field='link2']").textContent = object.link2;
+  clone.querySelector("[data-field='link2']").href = object.link2;
+
+  const dest = document.querySelector("article");
+
+  clone.querySelector("#closebutton").addEventListener("click", closeInfo);
+
+  dest.appendChild(clone);
+}
+
+function closeInfo() {
+  document.querySelector("h1").style.display = "block";
+  document.querySelector("article").style.display = "none";
+  document.querySelector("footer").style.display = "block";
+  document.querySelector("svg").style.height = "500px";
 }
