a5f11aed16d7ce1cc6a5136301379a7c58ca6684
Lene Frøkjær <lenefroekjaer@Lenes-MacBook-Pro-2.local>
2019-05-08 10:55:09 +0200

    displaying the svg

--- timeline.js
+++ timeline.js
@@ -1,1 +1,15 @@
 "use strict";
+window.addEventListener("DOMContentLoaded", loadSVG);
+
+//// HENTER SVG'EN
+
+function loadSVG() {
+  fetch("timeline.svg")
+    .then(response => response.text())
+    .then(svgdata => {
+      document
+        .querySelector("#svg_timeline")
+        .insertAdjacentHTML("afterbegin", svgdata);
+      chooseEvent();
+    });
+}
